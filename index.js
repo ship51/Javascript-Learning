6a71e69f5af2c27b67993fc0ad487f44f151e976
Shivam Pandit <spdata12@gmail.com>
2021-06-17 16:34:04 +0530

    2.4 Saving to database

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const express = require('express')
+const Datastore = require('nedb');
 const app = express()
 app.listen(3000, () => console.log('Listening at 3000'));
 app.use(express.static('public'));
 app.use(express.json({ limit: '1mb' }));
 
+const database = new Datastore('database.db');
+database.loadDatabase();
+
+// database.insert({ name: 'Chad', status: 'YES'});
+// database.insert({ name: 'Max', status: 'NO'});
+
 app.post('/api', (request, response) => {
     console.log('I got a request!');
     console.log(request.body);
     const data = request.body;
+    const timestamp = Date.now();
+    data.timestamp = timestamp;
+
+    database.insert(data);
+
     response.json({
         status: 'Success',
         latitude: data.lat,
-        longitude: data.long
+        longitude: data.long,
+        timestamp: timestamp
     });
 });
 
