b792f3c54c1d03b067bc2078420b66f99fcb79a1
Shivam Pandit <spdata12@gmail.com>
2021-06-28 23:33:38 +0530

    p5-sketch

---
+++ 11-Extensions/sketch/sketch.js
@@ -0,0 +1,15 @@
+
+console.log("Working in sketch.js");
+
+function setup(){
+
+    console.log("Working in Setup");
+    createCanvas(400, 400);
+    //c.position (0.0);
+}
+
+
+function draw(){
+    console.log("Working in Draw");
+    background(0);
+}
